742b33103741c16d07b4fef0461d07985bf88532
Adam Maciak <adam.maciak2@gmail.com>
2020-05-11 15:48:22 +0200

    rsa correct

--- src/main/java/algorytmRsa/algorytmRsa.java
+++ src/main/java/algorytmRsa/algorytmRsa.java
@@ -3,10 +3,11 @@ package algorytmRsa;
 import Euklides.Euklides;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class algorytmRsa {
 
-    static boolean isPrime(Integer number) {
+    static boolean isPrime(long number) {
         int count = 0;
         for (int i = 2; i < number; i++) {
             if (number % i == 0) count++;
@@ -15,7 +16,7 @@ public class algorytmRsa {
         return true;
     }
 
-    static long ModularExp(Long number, Long exponent, Long mod) {
+    static long ModularExp(long number, long exponent, long mod) {
         long res = 1;
         number %= mod;
         if (number == 0) return 0;
@@ -28,7 +29,7 @@ public class algorytmRsa {
         return res;
     }
 
-    static public long modInverse(long a, long b) {
+    static long modInverse(long a, long b) {
         long modulo = b;
         long t1 = 0, s1 = 1, t2 = 1, s2 = 0;
         while (a > 1) {
@@ -48,50 +49,105 @@ public class algorytmRsa {
         return s1;
     }
 
-    public static void main(String[] args) {
-        char[] messageToEncode = "Adam_Maciak".toCharArray();
-        long[] encodeMessage = new long[messageToEncode.length / 3 + 1];
-        char[] decodedMessage = new char[messageToEncode.length + 1];
+    static long[] changeBlocksCharToLong(char[] messageToEncode) {
         int position = 0;
-        for (int i = 0; i < messageToEncode.length; i = i + 3) {
+        int length = messageToEncode.length;
+        long[] encodeMessage = new long[length / 3 + 1];
+        for (int i = 0; i < length; i = i + 3) {
             encodeMessage[position] = ((long) messageToEncode[i]) << 16;
-            if (i + 1 < messageToEncode.length) encodeMessage[position] += ((long) messageToEncode[i + 1]) << 8;
-            if (i + 2 < messageToEncode.length) encodeMessage[position] += (long) messageToEncode[i + 2];
+            if (i + 1 < length) encodeMessage[position] += ((long) messageToEncode[i + 1]) << 8;
+            if (i + 2 < length) encodeMessage[position] += (long) messageToEncode[i + 2];
             position++;
+        }
+        return encodeMessage;
+    }
+
+    static long[] encode(long[] messageToEncode, long e, long n) {
+        int length = messageToEncode.length;
+        long[] encodeMessage = new long[length];
+        for (int i = 0; i < length; i++) {
+            encodeMessage[i] = ModularExp(messageToEncode[i], e, n);
+        }
+        return encodeMessage;
+    }
+
+    static long[] decode(long[] encodeMessage, long d, long n) {
+        int length = encodeMessage.length;
+        long[] decodedMessage = new long[length];
+        for (int i = 0; i < length; i++) {
+            decodedMessage[i] = ModularExp(encodeMessage[i], d, n);
+        }
+        return decodedMessage;
+    }
+
+    static char[] changeLongToCharBlock(long[] encodedMessageLong) {
+        int length = encodedMessageLong.length;
+        char[] decodedMessage = new char[length * 3];
+        int position = 0;
+        for (int i = 0; i < decodedMessage.length; i = i + 3) {
+            decodedMessage[i] = (char) ((encodedMessageLong[position] / (long) Math.pow(2, 16)) % (long) Math.pow(2,
+                    8));
+            if (i + 1 <= decodedMessage.length)
+                decodedMessage[i + 1] = (char) ((encodedMessageLong[position] / (long) Math.pow(2,
+                        8)) % (long) Math.pow(2, 8));
+            if (i + 2 <= decodedMessage.length)
+                decodedMessage[i + 2] = (char) (encodedMessageLong[position] % (long) Math.pow(2, 8));
+            position++;
+        }
+        return decodedMessage;
+    }
+
+    public static void main(String[] args) {
+        Scanner textFromConsole = new Scanner(System.in);
+       // char[] messageToEncode = "Adam_Maciak".toCharArray();
+        String message;
+        //p,q MUSZA BYC PROSTE, e też
+        //2707 ,2843,10273,5087
+        System.out.print("Podaj p = ");
+        long p = textFromConsole.nextLong();
+        System.out.print("Podaj q = ");
+        long q = textFromConsole.nextLong();
+        System.out.println("Czy p jest pierwsza? " + isPrime(p));
+        System.out.println("Czy q jest pierwsza? " + isPrime(q));
+        long n = p * q;
+        System.out.println("n = " + n);
+        long phi = (p - 1) * (q - 1);
+        System.out.println("phi = " + phi);
+        Random r = new Random();
+        long e = r.longs(1, phi - 1).filter(x -> isPrime(x)).findFirst().getAsLong();
+        System.out.println("losowane e jest rowne:" + e);
+        long d = modInverse(e, phi);
+        System.out.println("d = " + d);
+        System.out.print("podaj tekst = ");
+        message=textFromConsole.next();
+        System.out.println("tekst = " + message);
+        long[] a = changeBlocksCharToLong(message.toCharArray());
+        System.out.print("bloki = ");
+        for (long i :
+                a) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        long[] b = encode(a, e, n);
+        System.out.print("szyfr = ");
+        for (long i :
+                b) {
+            System.out.print(" " + i);
         }
 
-//        for (long i :
-//                encodeMessage) {
-//            System.out.println(i);
-//        }
-        long p = 3221;
-        long q = 2701;
-        long n = p * q;
-        long phi = (p - 1) * (q - 1);
-        long e = 3889;
-        long d = modInverse(e, phi);
-        System.out.println("wynik" + d);
-        for (int i = 0; i < encodeMessage.length; i++) {
-            encodeMessage[i] = ModularExp(encodeMessage[i], e, n);
-        }
+        System.out.println();
+        long[] c = decode(b, d, n);
+        System.out.print("odszyfrowane_bloki = ");
         for (long i :
-                encodeMessage) {
-            System.out.println(i);
+                c) {
+            System.out.print(" " + i);
         }
-        for (int i = 0; i < encodeMessage.length; i++) {
-            encodeMessage[i] = ModularExp(encodeMessage[i], d, n);
-        }
-//        for (long i :
-//                encodeMessage) {
-//            System.out.println(i);
-//        }
-        position = 0;
-        for (int i = 0; i < decodedMessage.length; i = i + 3) {
-            decodedMessage[i] = (char) ((encodeMessage[position] / (int) Math.pow(2, 16)) % Math.pow(2, 8));
-            decodedMessage[i + 1] = (char) ((encodeMessage[position] / (int) Math.pow(2, 8)) % Math.pow(2, 8));
-            decodedMessage[i + 2] = (char) (encodeMessage[position] % Math.pow(2, 8));
-            position++;
-        }
-        System.out.println(decodedMessage);
+
+        System.out.println();
+        char[] f = changeLongToCharBlock(c);
+        System.out.print("odszyfrowany_tekst = ");
+        System.out.print(f);
+
+        //System.out.println(changeLongToCharBlock(decode(encode(changeBlocksCharToLong(messageToEncode), e, n), d, n)));
     }
 }
